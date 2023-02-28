6f78b0707929f8cbca7f3689811b0d886b2ed57d
TotalAI Systems <79867019+totalaisystemsllp@users.noreply.github.com>
2023-02-28 16:35:27 +0530

    Add files via upload

---
+++ chatapp/database_config.js
@@ -0,0 +1,32 @@
+'use strict';
+module.exports = {
+
+   
+  mysql: {
+      dbcon_1: {
+          host: '3.17.123.98',
+          port: '3306',
+          username: 'chat',
+         password: 'Total@collectrchat123',
+         database: 'db_collectr'
+      },
+      dbcon_2: {
+        host: '3.17.123.98',
+        port: '3306',
+        username: 'chat',
+        password: 'Total@collectrchat123',
+        database: 'totalcr_io_db'
+    },
+      dbcon_11: {
+        host: '18.221.53.199',
+        port: '3306',
+        username: 'chat',
+        password: 'Totalminto@collectrchat123',
+        database: 'minto_db'
+    }
+
+     }
+
+  
+
+};
