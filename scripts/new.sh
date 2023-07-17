c10c2695d37b5cffbbab14c2732dca4f1c322b3a
TotalAI Systems <79867019+totalaisystemsllp@users.noreply.github.com>
2023-07-17 15:42:16 +0530

    Update new.sh
    Restart pm2 processes after deployment

--- scripts/new.sh
+++ scripts/new.sh
@@ -1,2 +1,6 @@
 #!/bin/bash 
-echo hi
+pm2 restart /home/app/chatapp/app.js;
+pm2 restart /home/app/chatapp/sms.js;
+pm2 restart /home/app/chatapp/is_online.js;
+pm2 restart /home/app/chatapp/messenger.js;
+pm2 restart /home/app/chatapp/whatsapp.js;
