b86ed4013574fb405bda508d167a12a4ce6df744
TotalAI Systems <79867019+totalaisystemsllp@users.noreply.github.com>
2023-07-17 15:28:43 +0530

    Update database_config.js
    Database_config updated

--- chatapp/database_config.js
+++ chatapp/database_config.js
@@ -1,7 +1,7 @@
-'use strict';
+\'use strict';
 module.exports = {
 
-   
+
   mysql: {
       dbcon_1: {
           host: '3.17.123.98',
@@ -17,16 +17,20 @@ module.exports = {
         password: 'Total@collectrchat123',
         database: 'totalcr_io_db'
     },
-      dbcon_11: {
-        host: '18.221.53.199',
+        dbcon_mfs: {
+        host: '3.17.199.120',
         port: '3306',
         username: 'chat',
-        password: 'Totalminto@collectrchat123',
-        database: 'minto_db'
-    }
+        password: 'thrBzIloFfvcn',
+        database: 'Total_mfs'
+       },
+       dbcon_beam: {
+        host: '3.133.229.163',
+        port: '3306',
+        username: 'chat',
+        password: 'f54nRGbTIDq38mC93CBgCLYR^4i',
+        database: 'beam_total_db'
+       }
 
      }
-
-  
-
 };
