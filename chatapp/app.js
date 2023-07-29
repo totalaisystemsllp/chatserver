52faa44e210f078ad61c9a06e0fb1ed4009ec791
vivek-totalai <119925791+vivek-totalai@users.noreply.github.com>
2023-07-29 12:52:37 +0530

    Update app.js

--- chatapp/app.js
+++ chatapp/app.js
@@ -84,7 +84,14 @@ io.on("connection", (socket) => {
       con.connect((err) => {
         if (err) {
           console.error("Error connecting to MySQL database:", err);
-          accessLogStream.write(JSON.stringify(err) + '\n');
+           logData = err+{
+            host: database.host,
+            user: database.username,
+            password: database.password,
+            database: database.database,
+            port: database.port,
+          };
+          accessLogStream.write(JSON.stringify(logData) + '\n');
           return;
         } else {
           
