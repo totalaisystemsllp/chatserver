7afb2b1d43f76c27e1fa6b35ac267f22aa87bca3
TotalAI Systems <79867019+totalaisystemsllp@users.noreply.github.com>
2024-03-07 22:01:53 +0530

    Update new.sh
    Whatsapp.js and messenger.js are not working, so commenting out

--- scripts/new.sh
+++ scripts/new.sh
@@ -2,5 +2,5 @@
 pm2 restart /home/app/chatapp/app.js;
 pm2 restart /home/app/chatapp/sms.js;
 pm2 restart /home/app/chatapp/is_online.js;
-pm2 restart /home/app/chatapp/messenger.js;
-pm2 restart /home/app/chatapp/whatsapp.js;
+#pm2 restart /home/app/chatapp/messenger.js;
+#pm2 restart /home/app/chatapp/whatsapp.js;
