6fb104bafea82d40d016e85db2c669a8f776aa92
vivek-totalai <vivek.kushwaha@totalaisystems.dev>
2024-03-08 10:48:17 +0530

    sms issue

--- chatapp/sms.js
+++ chatapp/sms.js
@@ -39,58 +39,53 @@ var sockets = {};
 var user_details = {};
 var rooms = {};
 var all_collector = [];
+var dbName = "COL_";
 
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(bodyParser.json());
 
-
 app.post("/", (req, res) => {
-
   console.log("data from post:", req.body);
 
   var data = req.body;
 
   if (data.is_accept == 0) {
-
     io.to("collector").emit("mgs_req_from_debtor", data);
     console.log("message request to all collector");
-
   } else if (data.is_accept == 1) {
-
     io.to(data.thread_id).emit("mgs_from_debtor", data);
     console.log("message to " + data.thread_id + " thread id");
   }
 
   res.sendStatus(200);
-
 });
 
-/**************Socket Connection**********************/ 
+/**************Socket Connection**********************/
 
 io.on("connection", (socket) => {
-
+  dbName += socket.handshake.query.db;
   socket.on(
     "register_user_id",
     function (userId, old_thread_list = [], role, name, email = null) {
-      sockets["COL_" + userId] = socket;
-      all_collector.push("COL_" + userId);
+      sockets[dbName + userId] = socket;
+      all_collector.push(dbName + userId);
       // array unique
       all_collector = all_collector.filter((x, i, a) => a.indexOf(x) == i);
-      user_details["COL_" + userId] = {
+      user_details[dbName + userId] = {
         userId: userId,
         role: role,
         name: name,
         email: email,
       };
-      sockets["COL_" + userId].join("collector");
+      sockets[dbName + userId].join("collector");
 
-      console.log("COL_" + userId + " user connected");
+      console.log(dbName + userId + " user connected");
       console.log(user_details);
       console.log(all_collector);
 
       for (var i = 0; i < old_thread_list.length; i++) {
-        sockets["COL_" + userId].join(old_thread_list[i]);
+        sockets[dbName + userId].join(old_thread_list[i]);
         console.log(
           "old room id " +
             old_thread_list[i] +
@@ -103,8 +98,8 @@ io.on("connection", (socket) => {
   );
 
   socket.on("req_accept", function (thread_id, coll_id) {
-    sockets["COL_" + coll_id].join(thread_id);
-    rooms[thread_id] = { coll_id: "COL_" + coll_id };
+    sockets[dbName + coll_id].join(thread_id);
+    rooms[thread_id] = { coll_id: dbName + coll_id };
     socket.to("collector").emit("remove_req_accept", thread_id);
     console.log(
       "new room id " +
@@ -126,8 +121,8 @@ io.on("connection", (socket) => {
     (thread_id, from_collector_id, to_collector_id) => {
       socket.leave(thread_id);
 
-      if (sockets.hasOwnProperty("COL_" + to_collector_id)) {
-        sockets["COL_" + to_collector_id].join(thread_id);
+      if (sockets.hasOwnProperty(dbName + to_collector_id)) {
+        sockets[dbName + to_collector_id].join(thread_id);
         io.to(thread_id).emit("transfered_chat", { thread_id: thread_id });
 
         console.log(
@@ -139,7 +134,7 @@ io.on("connection", (socket) => {
             thread_id
         );
 
-        // rooms[thread_id][coll_id]="COL_"+to_collector_id;
+        // rooms[thread_id][coll_id]=dbName+to_collector_id;
         // console.log(rooms[thread_id]);
       }
     }
