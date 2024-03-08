64b03779fdd6ca38e88be8fb4a4e180f071310ed
vivek-totalai <vivek.kushwaha@totalaisystems.dev>
2024-03-08 14:19:59 +0530

    revert changes

--- chatapp/sms.js
+++ chatapp/sms.js
@@ -4,25 +4,12 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const socketIO = require("socket.io");
 
-/****Get Configuration****/
-const config = require("./config");
-
 const app = express();
 const server = http.createServer(app);
 
+const config = require("./config");
 const allowedOrigins = config.allowedOrigins;
 
-/***** Socket Connection *****/
-const io = socketIO(server, {
-  allowEIO3: true,
-  cors: {
-    origin: allowedOrigins,
-    methods: ["GET", "POST"],
-    credentials: true,
-  },
-});
-
-/***** Handling Cors Connection *****/
 app.use(
   cors({
     origin: (origin, callback) => {
@@ -35,100 +22,137 @@ app.use(
   })
 );
 
-var sockets = {};
-var user_details = {};
-var rooms = {};
-var all_collector = [];
-var dbName = "";
-var roomName = "";
+let sockets = {};
+let user_details = {};
+let rooms = {};
+let all_collector = [];
 
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
-app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  console.log("Server is up");
+  res.sendStatus(200);
+});
 
 app.post("/", (req, res) => {
-  console.log("data from post:", req.body);
+  console.log("Data from POST:", req.body);
 
-  var data = req.body;
+  const data = req.body;
 
   if (data.is_accept == 0) {
     io.to("collector").emit("mgs_req_from_debtor", data);
-    console.log("message request to all collector");
+    console.log("Message request to all collectors");
   } else if (data.is_accept == 1) {
     io.to(data.thread_id).emit("mgs_from_debtor", data);
-    console.log("message to " + data.thread_id + " thread id");
+    console.log("Message to " + data.thread_id + " thread ID");
   }
 
   res.sendStatus(200);
 });
 
-/**************Socket Connection**********************/
+/*************socket Connection*****************/
+const io = socketIO(server, {
+  allowEIO3: true,
+  cors: {
+    origin: allowedOrigins,
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
 
 io.on("connection", (socket) => {
-  dbName = socket.handshake.query.db ?? "";
-  roomName = `ROOM_${dbName}`;
   socket.on(
     "register_user_id",
-    function (userId, old_thread_list = [], role, name, email = null) {
-      let scoketId = `${dbName}_${userId}`;
-      sockets[scoketId] = socket;
-      all_collector.push(scoketId);
-      // array unique
-      all_collector = all_collector.filter((x, i, a) => a.indexOf(x) == i);
-      user_details[scoketId] = {
+    (userId, old_thread_list = [], role, name, email = null) => {
+      const socketId = "COL_" + userId;
+      sockets[socketId] = socket;
+      all_collector.push(socketId);
+      all_collector = all_collector.filter((x, i, a) => a.indexOf(x) === i);
+      user_details[socketId] = {
         userId: userId,
         role: role,
         name: name,
         email: email,
       };
-      sockets[scoketId].join(roomName);
+      sockets[socketId].join("collector");
 
-      for (var i = 0; i < old_thread_list.length; i++) {
-        sockets[scoketId].join(old_thread_list[i]);
+      console.log(socketId + " user connected");
+      console.log(user_details);
+      console.log(all_collector);
+
+      for (let i = 0; i < old_thread_list.length; i++) {
+        sockets[socketId].join(old_thread_list[i]);
+        console.log(
+          "Old room ID " +
+            old_thread_list[i] +
+            " created and " +
+            socketId +
+            " user joined"
+        );
       }
     }
   );
 
-  socket.on("req_accept", function (thread_id, coll_id) {
-    let scoketId = `${dbName}_${coll_id}`;
-
-    sockets[scoketId].join(thread_id);
-    rooms[thread_id] = { coll_id: scoketId };
-
-    socket.to(roomName).emit("remove_req_accept", thread_id);
+  socket.on("req_accept", (thread_id, coll_id) => {
+    const socketId = "COL_" + coll_id;
+    sockets[socketId].join(thread_id);
+    rooms[thread_id] = { coll_id: socketId };
+    socket.to("collector").emit("remove_req_accept", thread_id);
+    console.log(
+      "New room ID " + thread_id + " created and " + socketId + " user joined"
+    );
   });
 
-  socket.on("close", function (thread_id) {
+  socket.on("close", (thread_id) => {
     delete rooms[thread_id];
+    console.log("Room ID " + thread_id + " closed");
   });
 
   socket.on(
     "transfer_chat",
     (thread_id, from_collector_id, to_collector_id) => {
+      const fromSocketId = "COL_" + from_collector_id;
+      const toSocketId = "COL_" + to_collector_id;
+
       socket.leave(thread_id);
 
-      let scoketId = `${dbName}_${coll_id}`;
-
-      if (sockets.hasOwnProperty(scoketId)) {
-        sockets[scoketId].join(thread_id);
+      if (sockets.hasOwnProperty(toSocketId)) {
+        sockets[toSocketId].join(thread_id);
         io.to(thread_id).emit("transfered_chat", { thread_id: thread_id });
+
+        console.log(
+          "Transfer chat from user ID " +
+            fromSocketId +
+            " to " +
+            toSocketId +
+            " and thread ID " +
+            thread_id
+        );
       }
     }
   );
 
   socket.on("disconnect", () => {
-    let key = Object.keys(sockets)[Object.values(sockets).indexOf(socket)];
+    const socketId = Object.keys(sockets).find(
+      (key) => sockets[key] === socket
+    );
+    if (socketId) {
+      console.log(socketId + " user ID is disconnected");
+      const index = all_collector.indexOf(socketId);
+      if (index > -1) {
+        all_collector.splice(index, 1);
+      }
 
-    const index = all_collector.indexOf(key);
-    if (index > -1) {
-      all_collector.splice(index, 1);
+      delete user_details[socketId];
+      delete sockets[socketId];
+      console.log(user_details);
+      console.log(all_collector);
     }
-
-    delete user_details[key];
-    delete sockets[key];
   });
 });
 
-server.listen(7000, function () {
-  console.log("listening on *:7000");
+const port = process.env.PORT || 7000;
+server.listen(port, () => {
+  console.log("Listening on port " + port);
 });
