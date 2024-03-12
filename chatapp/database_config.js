cb1f1652f8d43b4ba499c0af0e2ee348e879075a
TotalAI Systems <79867019+totalaisystemsllp@users.noreply.github.com>
2024-03-12 21:57:53 +0530

    Update database_config.js
    Removed unused instance

--- chatapp/database_config.js
+++ chatapp/database_config.js
@@ -3,20 +3,13 @@ module.exports = {
 
 
   mysql: {
-      dbcon_1: {
-          host: '3.17.123.98',
-          port: '3306',
-          username: 'chat',
+         dbcon_1: {
+         host: '3.17.123.98',
+         port: '3306',
+         username: 'chat',
          password: 'Total@collectrchat123new',
          database: 'db_collectr'
       },
-      dbcon_2: {
-        host: '3.17.123.98',
-        port: '3306',
-        username: 'chat',
-        password: 'Total@collectrchat123',
-        database: 'totalcr_io_db'
-    },
         dbcon_mfs: {
         host: '3.17.199.120',
         port: '3306',
@@ -24,7 +17,7 @@ module.exports = {
         password: 'thrBzIloFfvcn',
         database: 'Total_mfs'
        },
-       dbcon_beam: {
+        dbcon_beam: {
         host: '3.133.229.163',
         port: '3306',
         username: 'chat',
